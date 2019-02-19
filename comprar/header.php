8a248fc481948cafcdc4b81b1e8ecb4650cac01e
Gabriel Craveiro <gabriel.craveiro@intuiti.com.br>
2019-02-19 13:57:06 -0300

    #187 Alteração dos icones de deleção

--- comprar/header.php
+++ comprar/header.php
@@ -30,7 +30,6 @@ $rows = numRows($mainConnection, "SELECT 1 FROM MW_RESERVA WHERE ID_SESSION = ?"
 
 ?>
 
-<link href="https://fonts.googleapis.com/css?family=Gothic+A1" rel="stylesheet">
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
