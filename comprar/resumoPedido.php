d09e5a904b9476ff2c6893513ee3ea8c1686f078
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-07-01 16:30:42 -0300

    Alterações cadastro tixsme

--- comprar/resumoPedido.php
+++ comprar/resumoPedido.php
@@ -139,16 +139,15 @@ while ($rs = fetchResult($result)) {
 						<div class="img_complemento">
 							<img src="">
 						</div>
-						<div class="ajuda" style="display: flex; align-content: center; align-self: center" >
-						</div>
 						<div class="icone_validador <?php echo ($rs['CD_BINITAU'] || $rs['NR_BENEFICIO']) ? 'valido' : ''; ?>" style="margin: 0px 0 0 0px !important; padding-right: 30px !important"></div>
 						<div class="container_validador" style="display: flex; align-items: center; align-content: center">
 							<input type="text" name="bin[]" class="validador_itau form-control  <?php echo ($rs['CD_BINITAU'] || $rs['NR_BENEFICIO']) ? 'hidden' : 'notHidden'; ?>" placeholder="<?php echo $beneficio_texto; ?>" maxlength="<?php echo $beneficio_size; ?>" value="<?php echo $rs['CD_BINITAU'].$rs['NR_BENEFICIO']; ?>" <?php echo ($rs['CD_BINITAU'] || $rs['NR_BENEFICIO']) ? 'readonly' : ''; ?> style="    width: 100%;
     margin: 0px 10px 0 20px;">
 							<input type="hidden" name="tipoBin[]" value="<?php echo $rs['CD_BINITAU'] ? 'itau' : 'promocao'; ?>" />
-							<a style="text-decoration: underline;" class="validarBin <?php echo ($rs['CD_BINITAU'] || $rs['NR_BENEFICIO']) ? 'hidden' : 'notHidden'; ?>" href="#">validar</a>
+							
 							<!-- <img class="<?php// echo !($rs['CD_BINITAU'] || $rs['NR_BENEFICIO']) ? 'hidden' : 'notHidden'; ?>" src=""> -->
 						</div>
+						<a style="text-decoration: underline;" class=" validarBin <?php echo ($rs['CD_BINITAU'] || $rs['NR_BENEFICIO']) ? 'hidden' : 'notHidden'; ?>" href="#">validar</a>
 					</div>
 				</td>
 			</tr>
