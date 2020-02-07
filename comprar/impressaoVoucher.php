2efee8e39dc4a27b461d30fe46d543901e76f705
Giulio Martinelli <giulioaugustomartinelli@gmail.com>
2020-02-07 02:49:52 -0300

    correcao

--- comprar/impressaoVoucher.php
+++ comprar/impressaoVoucher.php
@@ -42,9 +42,6 @@ exec('USE '+ @ds_nome_base_sql + ';SELECT description_voucher from tabPeca where
 $params  = array($parametros['OrderData']['OrderId']);
 $voucher = executeSQL($mainConnection, $query, $params, true);
 
-var_dump($voucher);
-die();
-
 if($voucher){
     $voucher = $voucher["description_voucher"];
 } else {
