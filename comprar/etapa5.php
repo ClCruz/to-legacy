7d81a7ed59d281efab7e42228768ed8b9028584f
Arthur Rodrigues <arthurrg23@gmail.com>
2020-03-23 12:22:21 -0300

    voltando alert

--- comprar/etapa5.php
+++ comprar/etapa5.php
@@ -206,8 +206,8 @@ require_once("makeitmine.php");
 
 	<div id="pai">
 		<?php require "header.php"; ?>
-		<div id="overlay">
-			<div class="">
+		<div id="content">
+			<div class="alert">
 				<div class="centraliza">
 					<img src="../images/ico_erro_notificacao.svg">
 					<div class="container_erros"></div>
